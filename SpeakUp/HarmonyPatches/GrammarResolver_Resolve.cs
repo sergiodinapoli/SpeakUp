fde33bd03e0ee1232ae58bc13a341b5b874a6938
Matthew <mattadair39@gmail.com>
2021-09-14 21:42:24 -0400

    Force Rimworld to always use untranslated rules for language support
    Needs more testing

--- SpeakUp/HarmonyPatches/GrammarResolver_Resolve.cs
+++ SpeakUp/HarmonyPatches/GrammarResolver_Resolve.cs
@@ -12,14 +12,22 @@ namespace SpeakUp
     {
         private static FieldInfo rulesInfo = AccessTools.Field(typeof(GrammarRequest), "rules");
 
-        public static void Prefix(object __instance, string rootKeyword, GrammarRequest request)
+        public static bool Prefix(ref string __result, object __instance, string rootKeyword, GrammarRequest request, string debugLabel = null, bool forceLog = false, string untranslatedRootKeyword = null, List<string> extraTags = null, List<string> outTags = null, bool capitalizeFirstSentence = true)
         {
-            if (rootKeyword != "r_logentry") return;
+            if (rootKeyword != "r_logentry") return true;
             List<Rule> rules = (List<Rule>)rulesInfo.GetValue(request);
-            if (rules.NullOrEmpty()) return;
+            if (rules.NullOrEmpty()) return true;
             var newRules = ExtraGrammarUtility.ExtraRules();
-            if (newRules.EnumerableNullOrEmpty()) return;
+            if (newRules.EnumerableNullOrEmpty()) return true;
             rules.AddRange(newRules);
+
+            //Always use untranslated dialogues so patches folder is not overwritten by rimworld language files
+            if (LanguageDatabase.activeLanguage != LanguageDatabase.defaultLanguage)
+            {
+                __result = GrammarResolver.ResolveUnsafe(untranslatedRootKeyword ?? rootKeyword, request, debugLabel, forceLog, true, extraTags, outTags, capitalizeFirstSentence);
+                return false;
+            }
+            return true;
         }
     }
 }
